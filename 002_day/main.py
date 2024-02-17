bd2d85bdac1806e922343742b0c410993269ed14
Marty <riedlmarty@gmail.com>
2024-02-17 15:37:04 +0100

    Add my_function to main.py

---
+++ 002_day/main.py
@@ -0,0 +1,11 @@
+  
+  
+def my_function():
+    function_str = "This is a function"
+    print(function_str)
+    print(function_str)
+    print(function_str)
+    print(function_str)
+    print(function_str)
+
+my_function()
