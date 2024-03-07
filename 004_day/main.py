70ed84bcceb0660a7f4fc073633bf9d4088fc184
Marty <riedlmarty@gmail.com>
2024-03-07 16:46:38 +0100

    Add turtle drawing functionality

--- 004_day/main.py
+++ 004_day/main.py
@@ -1,47 +1,34 @@
-def multiply(x, y):
-    return x * y
+from turtle import Turtle, Screen
+import heroes 
+import random
 
-def add(x, y):
-    return x + y
+turtle = Turtle()
 
-def subtract(x, y):
-    return x - y
+turtle.speed(0)
 
-def divide(x, y):
-    return x / y
+def line(angle):
+    turtle.forward(15)
+    turtle.right(angle)
+    print(heroes.gen())
 
-operations = {
-    "*": multiply,
-    "+": add,
-    "-": subtract,
-    "/": divide
-}
 
-def calc(prev_result):
-    print("Enter an operation: ")
-    operation = input()
-    if operation in operations:
-        print("Enter a second number: ")
-        num = float(input())
-        result = operations[operation](prev_result, num)
-        print(f"Result: {result}")
-        return result
-    else:
-        print("Invalid operation")
-        return prev_result
-        
-        
-print("Welcome to the calculator")
-print("Enter a first number: ")
-number = float(input())   
-while True:
-    number = calc(number)
-    print("Do you want to continue? (y/n)")
-    cont = input()
-    if cont == "n":
-        break
-    elif cont == "y":
-        print("Your first number is: " + str(number))
-    else:
-        print("Invalid input")
-        break
+turtle.shape("turtle")
+
+
+# random wal
+for i in range(4, 1000):
+    for _ in range(i):
+        turtle.pencolor(random.random(), random.random(), random.random())
+        turtle.pensize(5)
+        line(random.choice([90, 180, 270, 360]))
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
+
