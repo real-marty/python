c1273531a3cb54ada00aca3e374a0b41460c9457
Marty <riedlmarty@gmail.com>
2024-03-07 16:44:51 +0100

    Add random walk feature to main.py

---
+++ 004_day/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+import heroes 
+import random
+
+turtle = Turtle()
+
+turtle.speed(0)
+
+def line(angle):
+    turtle.forward(15)
+    turtle.right(angle)
+    print(heroes.gen())
+
+
+turtle.shape("turtle")
+
+
+# random walk
+for i in range(4, 1000):
+    for _ in range(i):
+        turtle.pencolor(random.random(), random.random(), random.random())
+        turtle.pensize(5)
+        line(random.choice([90, 180, 270, 360]))
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
